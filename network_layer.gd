fe347e518317a3d960936c1718f002375a65891c
SaracenOne <SaracenOne@gmail.com>
2019-08-14 11:37:43 +0100

    Initial commit

---
+++ network_layer.gd
@@ -0,0 +1,159 @@
+extends Node
+
+const DEFAULT_PORT = 10567
+const MAX_PEERS = 32
+
+var player_name : String = "Player"
+var players : Dictionary = {}
+
+signal player_list_changed()
+signal connection_failed()
+signal connection_succeeded()
+signal game_ended()
+signal game_error(what)
+signal received_audio_packet(p_id, p_index, p_packet)
+
+static func encode_16_bit_value(p_value : int) -> PoolByteArray:
+	return PoolByteArray([(p_value & 0x000000ff), (p_value & 0x0000ff00) >> 8])
+
+static func decode_16_bit_value(p_buffer : PoolByteArray) -> int:
+	var integer : int = 0
+	integer = p_buffer[0] & 0x000000ff | (p_buffer[1] << 8) & 0x0000ff00
+	return integer
+
+static func encode_24_bit_value(p_value : int) -> PoolByteArray:
+	return PoolByteArray([(p_value & 0x000000ff), (p_value & 0x0000ff00) >> 8, (p_value & 0x00ff0000) >> 16])
+
+static func decode_24_bit_value(p_buffer : PoolByteArray) -> int:
+	var integer : int = 0
+	integer = p_buffer[0] & 0x000000ff | (p_buffer[1] << 8) & 0x0000ff00 | (p_buffer[2] << 16) & 0x00ff0000
+	return integer
+
+func _player_connected(p_id : int) -> void:
+	print(str(p_id) + " connected!")
+
+func _player_disconnected(id : int) -> void:
+	if get_tree().is_network_server():
+		unregister_player(id)
+		for p_id in players:
+			# Erase in the server
+			rpc_id(p_id, "unregister_player", id)
+
+func _connected_ok() -> void:
+	rpc("register_player", get_tree().get_network_unique_id(), player_name)
+	emit_signal("connection_succeeded")
+
+# Callback from SceneTree, only for clients (not server)
+func _server_disconnected() -> void:
+	emit_signal("game_error", "Server disconnected")
+	end_game()
+
+# Callback from SceneTree, only for clients (not server)
+func _connected_fail() -> void:
+	get_tree().set_network_peer(null) # Remove peer
+	emit_signal("connection_failed")
+
+func _network_peer_packet(p_id : int, packet : PoolByteArray) -> void:
+	var result : Array = decode_voice_packet(packet)
+	emit_signal("received_audio_packet", p_id, result[0], result[1])
+
+# Lobby management functions
+
+remote func register_player(id : int, new_player_name : String) -> void:
+	if get_tree().is_network_server():
+		rpc_id(id, "register_player", 1, player_name)
+		for p_id in players:
+			rpc_id(id, "register_player", p_id, players[p_id])
+			rpc_id(p_id, "register_player", id, new_player_name)
+
+	players[id] = new_player_name
+	emit_signal("player_list_changed")
+
+remote func unregister_player(p_id : int) -> void:
+	if players.erase(p_id) == true:
+		emit_signal("player_list_changed")
+	else:
+		printerr("unregister_player: invalid id " + str(p_id))
+
+func is_network_server():
+	return get_tree().is_network_server()
+
+func host_game(new_player_name : String) -> bool:
+	player_name = new_player_name
+	var host : NetworkedMultiplayerENet = NetworkedMultiplayerENet.new()
+	if host.create_server(DEFAULT_PORT, MAX_PEERS) == OK:
+		get_tree().set_network_peer(host)
+		return true
+	
+	return false
+
+func join_game(ip : String, new_player_name : String):
+	player_name = new_player_name
+	var host = NetworkedMultiplayerENet.new()
+	if host.create_client(ip, DEFAULT_PORT) == OK:
+		get_tree().set_network_peer(host)
+
+func get_player_list() -> Array:
+	return players.values()
+	
+func get_player_ids() -> Array:
+	return players.keys()
+
+func get_player_name() -> String:
+	return player_name
+
+func end_game():
+	emit_signal("game_ended")
+	players.clear()
+	get_tree().set_network_peer(null) # End networking
+	
+func encode_voice_packet(p_index : int, p_voice_buffer : PoolByteArray) -> PoolByteArray:
+	var encoded_index : PoolByteArray = encode_24_bit_value(p_index)
+	var encoded_size : PoolByteArray = encode_16_bit_value(p_voice_buffer.size())
+	
+	var new_pool = PoolByteArray()
+	new_pool.append_array(encoded_index)
+	new_pool.append_array(encoded_size)
+	new_pool.append_array(p_voice_buffer)
+	
+	return new_pool
+	
+func decode_voice_packet(p_voice_buffer : PoolByteArray) -> Array:
+	var new_pool : PoolByteArray = PoolByteArray()
+	var encoded_id : int = -1
+	
+	if p_voice_buffer.size() > 5:
+		var index : int = 0
+		encoded_id = decode_24_bit_value(PoolByteArray([p_voice_buffer[index + 0], p_voice_buffer[index + 1], p_voice_buffer[index + 2]]))
+		index += 3
+		
+		var encoded_size : int = decode_16_bit_value(PoolByteArray([p_voice_buffer[index + 0], p_voice_buffer[index + 1]]))
+		index += 2
+		
+		new_pool = p_voice_buffer.subarray(index, index + (encoded_size - 1))
+		
+		
+	return [encoded_id, new_pool]
+
+func send_audio_packet(p_index : int, p_data : PoolByteArray) -> void:
+	var compressed_audio_packet = encode_voice_packet(p_index , p_data)
+	if get_tree().multiplayer.send_bytes(compressed_audio_packet, NetworkedMultiplayerPeer.TARGET_PEER_BROADCAST, NetworkedMultiplayerPeer.TRANSFER_MODE_UNRELIABLE) != OK:
+		printerr("send_audio_packet: send_bytes failed!")
+
+func _ready() -> void:
+	var connect_result : int = OK
+	
+	if get_tree().connect("network_peer_connected", self, "_player_connected") != OK:
+		printerr("could not connect network_peer_connected!")
+	if get_tree().connect("network_peer_disconnected", self,"_player_disconnected") != OK:
+		printerr("could not connect network_peer_disconnected!")
+	if get_tree().connect("connected_to_server", self, "_connected_ok") != OK:
+		printerr("could not connect connected_to_server!")
+	if get_tree().connect("connection_failed", self, "_connected_fail") != OK:
+		printerr("could not connect connection_failed!")
+	if get_tree().connect("server_disconnected", self, "_server_disconnected") != OK:
+		printerr("could not connect server_disconnected!")
+	
+	connect_result = get_tree().multiplayer.connect("network_peer_packet", self, "_network_peer_packet")
+	if connect_result != OK:
+		printerr("NetworkManager: network_peer_packet could not be connected!")
---
+++ voice_controller.gd
@@ -0,0 +1,102 @@
+extends Node
+
+const voice_manager_const = preload("voice_manager_constants.gd")
+var blank_packet = PoolVector2Array()
+var player_audio = {}
+
+onready var godot_voice = get_node("../GodotVoice")
+
+func get_required_packet_count(p_playback : AudioStreamPlayback, p_frame_size : int) -> int:
+	var to_fill = p_playback.get_frames_available()
+	var required_packets = 0
+	while to_fill >= p_frame_size:
+		to_fill -= p_frame_size
+		required_packets += 1
+		
+	return required_packets
+
+func add_player_audio(p_player_id : int) -> void:
+	if !player_audio.has(p_player_id):
+		
+		var new_generator = AudioStreamGenerator.new()
+		new_generator.set_mix_rate(48000)
+		new_generator.set_buffer_length(0.04)
+		
+		var audio_stream_player = AudioStreamPlayer.new()
+		audio_stream_player.set_name(str(p_player_id))
+		audio_stream_player.set_stream(new_generator)
+		
+		$PlayerStreamPlayers.add_child(audio_stream_player)
+		audio_stream_player.play()
+		
+		player_audio[p_player_id] = {"audio_stream_player":audio_stream_player, "buffers":[], "index":-1}
+	
+func remove_player_audio(p_player_id : int) -> void:
+	if player_audio.has(p_player_id):
+		var audio_stream_player = player_audio[p_player_id].audio_stream_player
+		audio_stream_player.queue_free()
+		audio_stream_player.get_parent().remove_child(audio_stream_player)
+		player_audio.erase(p_player_id)
+
+func on_received_audio_packet(p_id, p_index, p_packet):
+	if player_audio.has(p_id):
+		var current_index = player_audio[p_id].index
+		var buffers = player_audio[p_id].buffers
+		
+		var index_offset = p_index - current_index
+		
+		if index_offset > 0:
+			# For skipped buffers, add empty packets
+			for i in range(0, index_offset-1):
+				buffers.push_front(null)
+			# Add the new valid buffer
+			buffers.push_front(p_packet)
+				
+			player_audio[p_id].index += index_offset
+		else:
+			var index = buffers.size() + index_offset
+			if index >= 0:
+				# Update existing buffer
+				buffers[index] = p_packet
+		
+		player_audio[p_id].buffers = buffers
+
+func update_player_audio():
+	var player_ids = network_layer.get_player_ids()
+	# Create stream players and buffer for any new players
+	for player_id in player_ids:
+		if !player_audio.has(player_id):
+			add_player_audio(player_id)
+			
+	# Remove stream players and buffer for any absent players
+	for player_id in player_audio.keys():
+		if !player_ids.has(player_id):
+			remove_player_audio(player_id)
+			
+func attempt_to_feed_stream(p_audio_stream_player, p_buffers):
+	var playback = p_audio_stream_player.get_stream_playback()
+	var required_packets = get_required_packet_count(playback, voice_manager_const.BUFFER_FRAME_COUNT)
+	
+	print(required_packets)
+	while p_buffers.size() < required_packets:
+		p_buffers.push_front(null)
+	
+	for i in range(0, required_packets):
+		var buffer = p_buffers.pop_back()
+		if buffer != null:
+			var uncompressed_audio : PoolVector2Array = godot_voice.decompress_buffer(buffer)
+			if uncompressed_audio.size() == voice_manager_const.BUFFER_FRAME_COUNT and network_layer.is_network_server():
+				playback.push_buffer(uncompressed_audio)
+			else:
+				playback.push_buffer(blank_packet)
+		else:
+			playback.push_buffer(blank_packet)
+
+func _process(delta):
+	if delta > 0.0:
+		for key in player_audio.keys():
+			attempt_to_feed_stream(player_audio[key].audio_stream_player, player_audio[key].buffers)
+
+func _init():
+	for i in range(0, voice_manager_const.BUFFER_FRAME_COUNT):
+		blank_packet.push_back(Vector2(0.0, 0.0))
